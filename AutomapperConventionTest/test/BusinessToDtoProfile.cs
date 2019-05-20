b8c77a1a9617cf6c0d0fa5be9d0c7a6ba0366649
Miroslav Bartl <18615545+czmotig@users.noreply.github.com>
2019-05-20 14:13:48 +0200

    split classes into their own files

--- AutomapperConventionTest/test/BusinessToDtoProfile.cs
+++ AutomapperConventionTest/test/BusinessToDtoProfile.cs
@@ -21,29 +21,8 @@
                     return s.Name == "T_" + d.Name.Underscore().ToUpperInvariant();
                 });
 
-
             SourceMemberNamingConvention = new PascalCaseNamingConvention();
             DestinationMemberNamingConvention = new UppercaseUnderscoreNamingConvention();
-        }
-    }
-
-    internal class DtoToBusinessProfile : Profile
-    {
-        public DtoToBusinessProfile()
-        {
-            CreateMap<int, bool>().ConvertUsing(val => val == 0 ? false : true);
-            CreateMap<int, AccountId>().ConvertUsing(id => (AccountId)(id.ToString()));
-            CreateMap<int, ProfileId>().ConvertUsing(id => (ProfileId)(id.ToString()));
-            CreateMap<int?, ProfileId?>().ConvertUsing(id => id.HasValue ? new ProfileId(id.Value.ToString()) : (ProfileId?)null);
-
-            AddConditionalObjectMapper()
-                .Where((s, d) =>
-                {
-                    return s.Name == d.Name.Substring(2).Pascalize();
-                });
-
-            SourceMemberNamingConvention = new UppercaseUnderscoreNamingConvention();
-            DestinationMemberNamingConvention = new PascalCaseNamingConvention();
         }
     }
 }
---
+++ AutomapperConventionTest/test/DtoToBusinessProfile.cs
@@ -0,0 +1,26 @@
+﻿namespace test
+{
+    using AutoMapper;
+    using AutoMapper.Mappers;
+    using Humanizer;
+
+    internal class DtoToBusinessProfile : Profile
+    {
+        public DtoToBusinessProfile()
+        {
+            CreateMap<int, bool>().ConvertUsing(val => val == 0 ? false : true);
+            CreateMap<int, AccountId>().ConvertUsing(id => (AccountId)(id.ToString()));
+            CreateMap<int, ProfileId>().ConvertUsing(id => (ProfileId)(id.ToString()));
+            CreateMap<int?, ProfileId?>().ConvertUsing(id => id.HasValue ? new ProfileId(id.Value.ToString()) : (ProfileId?)null);
+
+            AddConditionalObjectMapper()
+                .Where((s, d) =>
+                {
+                    return s.Name == d.Name.Substring(2).Pascalize();
+                });
+
+            SourceMemberNamingConvention = new UppercaseUnderscoreNamingConvention();
+            DestinationMemberNamingConvention = new PascalCaseNamingConvention();
+        }
+    }
+}
