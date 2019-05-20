bfa23c4f340fdcf77909f83b39a57bddea847186
Miroslav Bartl <18615545+czmotig@users.noreply.github.com>
2019-05-20 14:25:42 +0200

    Ibargaoanu advice

--- AutomapperConventionTest/test/BusinessToDtoProfile.cs
+++ AutomapperConventionTest/test/BusinessToDtoProfile.cs
@@ -9,20 +9,29 @@
     {
         public BusinessToDtoProfile()
         {
-            CreateMap<bool, int>().ConvertUsing((bl, nt) => bl ? 1 : 0);
-            
-            CreateMap<AccountId, int>().ConvertUsing(id => Int32.Parse(id.Value));
-            CreateMap<ProfileId, int>().ConvertUsing(id => Int32.Parse(id.Value));
-            CreateMap<ProfileId?, int?>().ConvertUsing(id => id.HasValue ? Int32.Parse(id.Value.Value) : (int?)null);
-
             AddConditionalObjectMapper()
                 .Where((s, d) =>
                 {
                     return s.Name == "T_" + d.Name.Underscore().ToUpperInvariant();
                 });
 
+            ForAllPropertyMaps(pm =>
+            {
+                //breakpoint here
+                return pm.SourceMember.Name == pm.DestinationMember.Name.Underscore().ToUpperInvariant();
+            }, (pm, o) =>
+            {
+                //do nothing
+            });
+            /*
+            CreateMap<bool, int>().ConvertUsing((bl, nt) => bl ? 1 : 0);
+            CreateMap<AccountId, int>().ConvertUsing(id => Int32.Parse(id.Value));
+            CreateMap<ProfileId, int>().ConvertUsing(id => Int32.Parse(id.Value));
+            CreateMap<ProfileId?, int?>().ConvertUsing(id => id.HasValue ? Int32.Parse(id.Value.Value) : (int?)null);
+            
             SourceMemberNamingConvention = new PascalCaseNamingConvention();
             DestinationMemberNamingConvention = new UppercaseUnderscoreNamingConvention();
+            */
         }
     }
 }
--- AutomapperConventionTest/test/DtoToBusinessProfile.cs
+++ AutomapperConventionTest/test/DtoToBusinessProfile.cs
@@ -8,19 +8,30 @@
     {
         public DtoToBusinessProfile()
         {
-            CreateMap<int, bool>().ConvertUsing(val => val == 0 ? false : true);
-            CreateMap<int, AccountId>().ConvertUsing(id => (AccountId)(id.ToString()));
-            CreateMap<int, ProfileId>().ConvertUsing(id => (ProfileId)(id.ToString()));
-            CreateMap<int?, ProfileId?>().ConvertUsing(id => id.HasValue ? new ProfileId(id.Value.ToString()) : (ProfileId?)null);
-
+         
             AddConditionalObjectMapper()
                 .Where((s, d) =>
                 {
                     return s.Name == d.Name.Substring(2).Pascalize();
                 });
 
+            ForAllPropertyMaps(pm =>
+            {
+                //breakpoint here
+                return pm.SourceMember.Name == pm.DestinationMember.Name.Underscore().ToUpperInvariant();
+            }, (pm, o) =>
+            {
+                //do nothing
+            });
+
+            /*
+            CreateMap<int, bool>().ConvertUsing(val => val == 0 ? false : true);
+            CreateMap<int, AccountId>().ConvertUsing(id => (AccountId)(id.ToString()));
+            CreateMap<int, ProfileId>().ConvertUsing(id => (ProfileId)(id.ToString()));
+            CreateMap<int?, ProfileId?>().ConvertUsing(id => id.HasValue ? new ProfileId(id.Value.ToString()) : (ProfileId?)null);
             SourceMemberNamingConvention = new UppercaseUnderscoreNamingConvention();
             DestinationMemberNamingConvention = new PascalCaseNamingConvention();
+            */
         }
     }
 }
